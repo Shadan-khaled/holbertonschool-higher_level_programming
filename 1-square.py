56e1aebd604e60083a8da9826fac23ad93b190fe
Shadan-khaled <Shadankhaled75@gmail.com>
2025-12-13 18:33:10 +0000

    Add

---
+++ 1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+
+class Square:
+    """Square with private size attribute."""
+
+    def __init__(self, size):
+        """Initialize square with size."""
+        self.__size = size
