272c6e0c7aef8359e3057a4fe049bdf851e4fb81
Shadan-khaled <Shadankhaled75@gmail.com>
2025-12-20 17:32:14 +0000

    add

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+5-base_geometry.py
+Defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class representing the base geometry.
+    """
+
+    pass
