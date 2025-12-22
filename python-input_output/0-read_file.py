392c1a0621e49821aa1f99ec7eb46de49662c79b
Shadan-khaled <Shadankhaled75@gmail.com>
2025-12-22 07:23:53 +0000

    Add

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module provides a function to read a text file
+and print its content to standard output.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): name of the file to read
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
