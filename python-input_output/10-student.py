d2906fb9e3ec37f0b640bdca1089687ca40beea1
Shadan-khaled <Shadankhaled75@gmail.com>
2025-12-24 19:27:24 +0000

    add

---
+++ python-input_output/10-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Defines a Student class with public attributes and
+a method to return a JSON-serializable dictionary,
+optionally filtered by a list of attribute names.
+"""
+
+
+class Student:
+    """
+    Student class with first_name, last_name, age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance
+
+        Args:
+            first_name (str)
+            last_name (str)
+            age (int)
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation of the Student instance.
+        If attrs is a list of strings, only attributes contained
+        in this list are retrieved.
+
+        Args:
+            attrs (list, optional): list of attribute names to include
+
+        Returns:
+            dict: dictionary representation of the Student instance
+        """
+        if attrs is None:
+            return self.__dict__.copy()
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
