90962ccde1394173b6a26c727ccb93ffdb4a6a6d
Shadan-khaled <Shadankhaled75@gmail.com>
2025-12-24 19:14:59 +0000

    Add

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module provides a function that creates a Python object
+from a JSON file.
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    Args:
+        filename (str): name of the JSON file
+
+    Returns:
+        object: Python data structure
+    """
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
